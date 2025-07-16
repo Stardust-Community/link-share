4a18ea081f5dab9cd46c5c51527ca45b3d78655c
Stardust <arkasarkar235@gmail.com>
2025-07-17 01:13:24 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,4 @@
-# +++ Modified By Yato [telegram username: @i_killed_my_clan & @ProYato] +++ # aNDI BANDI SANDI JISNE BHI CREDIT HATAYA USKI BANDI RAndi 
+# By arka [telegram username: @name_huh]    
 import asyncio
 import base64
 import time
@@ -16,7 +16,7 @@ from plugins.newpost import revoke_invite_after_5_minutes
 from helper_func import *
 
 # Start pic
-START_PIC_FILE_ID = "https://telegra.ph/file/f3d3aff9ec422158feb05-d2180e3665e0ac4d32.jpg"
+START_PIC_FILE_ID = "https://i.ibb.co/jvGxrgtD/x.jpg"
 
 user_banned_until = {}
 
