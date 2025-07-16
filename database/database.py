9c63a9e72b00769e21aef5ea313ba81913303207
Stardust <arkasarkar235@gmail.com>
2025-07-17 00:56:52 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,4 +1,4 @@
-# +++ Modified By Yato [telegram username: @i_killed_my_clan & @ProYato] +++ # aNDI BANDI SANDI JISNE BHI CREDIT HATAYA USKI BANDI RAndi 
+# +++ Modified By Yato [telegram username: @name_huh]  
 import motor.motor_asyncio
 import base64
 from config import DB_URI, DB_NAME
